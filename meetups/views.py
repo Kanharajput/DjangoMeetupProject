b47f6b8d28bf9a13f3e8210b2d1ae88b746827d6
Kanha Tomar <kanharajput91@gmail.com>
2023-01-13 17:10:10 +0530

    template created and styled

--- meetups/views.py
+++ meetups/views.py
@@ -3,14 +3,23 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    data = [
-        {"title":"First Meetup"},
-        {"title":"Second Meetup"}
-    ]
+    meetups_data = [
+            {
+                "title":"First Meetup",
+                "location":"Barda Punarwas",
+                "slug":"first-meetup"
+            },
+            {
+                "title":"Second Meetup",
+                "location":"Bakaner",
+                "slug":"second-meetup"
+            }
+        ]
+        
     # always pass a single dictionary in render functions 
     # as there is only single dictionary acceptance is there 
     context = {
-        "all_meetups":data,
+        "all_meetups":meetups_data,
         "show_meetups_data":False
     }
     return render(request,
