bdd452cc5d03cb6e60370a7d93618d476fdf43f0
Kanha Tomar <kanharajput91@gmail.com>
2023-01-14 15:19:36 +0530

    models created

--- meetups/models.py
+++ meetups/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Meetup(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=300)
+    location = models.CharField(max_length=50)
+    address = models.CharField(max_length=30)
+    slug = models.SlugField()
