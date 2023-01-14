fd9441547ccb52e7ad647cbf819f6c9e885fd46c
Kanha Tomar <kanharajput91@gmail.com>
2023-01-14 16:41:08 +0530

    prepopulate the slug

--- meetups/models.py
+++ meetups/models.py
@@ -8,6 +8,7 @@ class Meetup(models.Model):
     location = models.CharField(max_length=50)
     address = models.CharField(max_length=30)
     slug = models.SlugField()
+    
 
     def __str__(self):
         return self.title
