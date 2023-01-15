7e26f4f82ac4f331e56f6dc7a3865019c96ab052
Kanha Tomar <kanharajput91@gmail.com>
2023-01-15 12:01:36 +0530

    many to many relation

--- meetups/models.py
+++ meetups/models.py
@@ -11,12 +11,23 @@ class Location(models.Model):
         return f"{self.name} {self.address}"
 
 
+class Participants(models.Model):
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name_plural = "Participants"
+
+
 class Meetup(models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=300)
     slug = models.SlugField()
     image = models.ImageField(upload_to="images",null=True)       # upload images inside images folder inside MediaUploads folder
     place = models.ForeignKey(Location,on_delete=models.CASCADE,null=True)       # one to many
+    participants = models.ManyToManyField(Participants)       # doesn't need to set on_delete and null=True in many to many fields
     
     def __str__(self):
         return self.title
