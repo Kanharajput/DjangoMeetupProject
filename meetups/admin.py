32f7cd732767432d5ee4676a6f08b2b7ead73d38
Kanha Tomar <kanharajput91@gmail.com>
2023-01-15 14:02:12 +0530

    one to one relation

--- meetups/admin.py
+++ meetups/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Meetup,Location,Participants
+from .models import Meetup,Location,Participants,Organizer
 # Register your models here.
 
 class MeetupAdmin(admin.ModelAdmin):
@@ -10,3 +10,4 @@ class MeetupAdmin(admin.ModelAdmin):
 admin.site.register(Meetup,MeetupAdmin)
 admin.site.register(Location)
 admin.site.register(Participants)
+admin.site.register(Organizer)
--- meetups/models.py
+++ meetups/models.py
@@ -21,13 +21,23 @@ class Participants(models.Model):
         verbose_name_plural = "Participants"
 
 
+class Organizer(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+
 class Meetup(models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=300)
     slug = models.SlugField()
     image = models.ImageField(upload_to="images",null=True)       # upload images inside images folder inside MediaUploads folder
+    date_time = models.DateTimeField(auto_now=True)
     place = models.ForeignKey(Location,on_delete=models.CASCADE,null=True)       # one to many
     participants = models.ManyToManyField(Participants)       # doesn't need to set on_delete and null=True in many to many fields
+    organizer = models.OneToOneField(Organizer,on_delete=models.CASCADE,null=True)
     
     def __str__(self):
         return self.title
