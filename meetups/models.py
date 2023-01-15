d4c3dc4fa4ffe5000c33590d126855b50a18d592
Kanha Tomar <kanharajput91@gmail.com>
2023-01-15 11:42:02 +0530

    one to many relation

--- meetups/models.py
+++ meetups/models.py
@@ -2,14 +2,21 @@ from django.db import models
 
 # Create your models here.
 
+# one to many , as many meetups can happen at same location
+class Location(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.name} {self.address}"
+
+
 class Meetup(models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=300)
-    location = models.CharField(max_length=50)
-    address = models.CharField(max_length=30)
     slug = models.SlugField()
     image = models.ImageField(upload_to="images",null=True)       # upload images inside images folder inside MediaUploads folder
+    place = models.ForeignKey(Location,on_delete=models.CASCADE,null=True)       # one to many
     
-
     def __str__(self):
         return self.title
